44461065b71d3780aaefacd673552cce6950f186
chrisoney <christopher.oney@yahoo.com>
2020-06-22 13:45:55 -0700

    First push for user auth

---
+++ app/views/api/users/_user.json.builder
@@ -0,0 +1,1 @@
+json.extract! user, :id, :username, :guild_name, :guild_description
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/adventr_entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
