fdf279510cd85d575eb3cb1db2a3e9b89c1c5fe8
chrisoney <christopher.oney@yahoo.com>
2020-07-23 15:01:54 -0700

    updated package.json

--- frontend/adventr_entry.jsx
+++ frontend/adventr_entry.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, logout, signup } from './actions/session_actions';
 import configureStore from './store/store'
 import Root from './components/root'
 
@@ -23,8 +22,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.logout = logout;
 });
