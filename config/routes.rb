4c85d0dc2eee50349e3c53fc2c51ffe155928515
chrisoney <christopher.oney@yahoo.com>
2020-09-29 20:24:52 -0700

    add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :create, :update, :show]
+    resources :users, only: [:new, :index, :create, :edit, :update, :show]
     resource :session, only: [:create, :destroy]
-    resources :quests, only: [:index, :show, :new, :create, :update, :destroy]
+    resources :quests, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     resources :likes, only: [:index, :show, :create, :destroy]
     resources :follows, only: [:index, :show, :create, :destroy]
   end
