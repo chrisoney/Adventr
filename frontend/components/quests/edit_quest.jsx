92b9d37f7d1f18cb3219d90dd4a799c235d13882
chrisoney <christopher.oney@yahoo.com>
2020-09-30 19:55:47 -0700

    edit post container

---
+++ frontend/components/quests/edit_quest.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { updatePost } from '../../actions/post_actions';
+import { openModal, closeModal } from '../../actions/modal_actions';
+import { Link, withRouter } from 'react-router-dom';
+import EditPostForm from './edit_post_form';
+import { deleteAttachment } from '../../utils/post_api_utils';
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		currentUser: state.session.currentUser,
+		post: ownProps.post
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updatePost: (post) => dispatch(updatePost(post)),
+		deleteAttachment: (blobId)=> dispatch(deleteAttachment(blobId))
+	}
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(EditPostForm));
