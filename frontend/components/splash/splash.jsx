6bfc15877dc100601db0360dc1b226e77dd591e1
chrisoney <christopher.oney@yahoo.com>
2021-03-11 21:47:38 -0800

    splash page refactor with sidebar

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,25 +2,47 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.sidebarClick = this.sidebarClick.bind(this);
+  }
+
+  sidebarClick(e) {
+    e.preventDefault();
+    document.querySelectorAll('fa-circle').forEach((circle) => {
+      circle.classList.remove('fas').add('far');
+    });
+    e.target.classList.remove('far').add('fas');
+  }
   render() {
     return (
-      <div className="full-page">
-        <div className="splash">
-          <div className="splash-content">
-            <p className="app-name">adventr</p>
-            <div className="welcome-text">
-              <p>Come for the adventures.</p>
-              <p>Stay for the magic items.</p>
+      <>
+        <div className="sidebar">
+          <span className="fas fa-circle"></span>
+          <span className="far fa-circle"></span>
+          <span className="far fa-circle"></span>
+          <span className="far fa-circle"></span>
+          <span className="far fa-circle"></span>
+        </div>
+        <div className="full-page">
+          <div className="splash">
+            <div className="splash-content">
+              <p className="app-name">adventr</p>
+              <div className="welcome-text">
+                <p>Come for the adventures.</p>
+                <p>Stay for the magic items.</p>
+              </div>
+              <Link to="/signup">
+                <button className="signup">Get Started</button>
+              </Link>
+              <Link to="/login">
+                <button className="login">Login</button>
+              </Link>
             </div>
-            <Link to="/signup">
-              <button className="signup">Get Started</button>
-            </Link>
-            <Link to="/login">
-              <button className="login">Login</button>
-            </Link>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
