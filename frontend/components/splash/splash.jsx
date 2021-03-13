d7356611679b5ae90412d4fb575a5bd283c085e9
chrisoney <christopher.oney@yahoo.com>
2021-03-12 20:23:07 -0800

    fix background animation

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -26,8 +26,14 @@ class Splash extends React.Component {
     return (
       <div className="splash-container">
         <div className="sidebar">
-          <span className="fas fa-circle" onClick={(e) => this.handleScroll(e, 'splash-1')}></span>
-          <span className="far fa-circle" onClick={(e) => this.handleScroll(e, 'splash-2')}></span>
+          <span
+            className="fas fa-circle"
+            onClick={(e) => this.handleScroll(e, 'splash-1')}
+          ></span>
+          <span
+            className="far fa-circle"
+            onClick={(e) => this.handleScroll(e, 'splash-2')}
+          ></span>
           <span
             className="far fa-circle"
             onClick={(e) => this.sidebarClick(e)}
@@ -45,29 +51,25 @@ class Splash extends React.Component {
             onClick={(e) => this.sidebarClick(e)}
           ></span>
         </div>
-        <div className="full-page">
-          <div className="splash" id="splash-1">
-            <div className="splash-content">
-              <p className="app-name">adventr</p>
-              <div className="welcome-text">
-                <p>Come for the adventures.</p>
-                <p>Stay for the magic items.</p>
-              </div>
-              <Link to="/signup">
-                <button className="signup">Get Started</button>
-              </Link>
-              <Link to="/login">
-                <button className="login">Login</button>
-              </Link>
+        <div className="splash" id="splash-1">
+          <div className="splash-content">
+            <p className="app-name">adventr</p>
+            <div className="welcome-text">
+              <p>Come for the adventures.</p>
+              <p>Stay for the magic items.</p>
             </div>
+            <Link to="/signup">
+              <button className="signup">Get Started</button>
+            </Link>
+            <Link to="/login">
+              <button className="login">Login</button>
+            </Link>
+          </div>
+          <div className="bottom-section">
+            <div className="bottom-text">What is Adventr?</div>
           </div>
         </div>
-        <div className="bottom-section">
-          <div className="bottom-text">What is Adventr?</div>
-        </div>
-        <div className="full-page" id="splash-2">
-          <div className="splash-2"></div>
-        </div>
+        <div className="splash" id="splash-2"></div>
       </div>
     );
   }
