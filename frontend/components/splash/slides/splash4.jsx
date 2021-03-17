8705223b2c25730af3fc05e4ea05bf077a0eedbb
chrisoney <christopher.oney@yahoo.com>
2021-03-16 20:38:52 -0700

    add skeleton for splash slides 4 through 6

---
+++ frontend/components/splash/slides/splash4.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Splash4 extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="splash4-content">
+          <div className="splash4-text">
+            <div className="splash4-hook">Adventr is quests</div>
+            <div className="splash4-description">
+              Turns out that when you make it easy to sign up for quests, those
+              quests actually get completed. All those dangerous, worthwhile
+              missions your companions beg to go on, those are Adventr quests.
+              We'll help you discover quests like that, and we'll help other
+              adventurers find and take on yours.
+            </div>
+          </div>
+          <div className="splash4-image">Place Image here</div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Splash4;
---
+++ frontend/components/splash/slides/splash5.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Splash5 extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="splash5-content">
+          <div className="splash5-text">
+            <div className="splash5-hook">Adventr is quests</div>
+            <div className="splash5-description">
+              Turns out that when you make it easy to sign up for quests, those
+              quests actually get completed. All those dangerous, worthwhile
+              missions your companions beg to go on, those are Adventr quests.
+              We'll help you discover quests like that, and we'll help other
+              adventurers find and take on yours.
+            </div>
+          </div>
+          <div className="splash5-image">Place Image here</div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Splash5;
