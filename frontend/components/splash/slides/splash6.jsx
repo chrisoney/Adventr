66de0c914bf8ac38402135ec8fb7f4ef453bf6e5
chrisoney <christopher.oney@yahoo.com>
2021-03-16 20:51:44 -0700

    splash 6 text content

--- frontend/components/splash/slides/splash6.jsx
+++ frontend/components/splash/slides/splash6.jsx
@@ -4,18 +4,21 @@ class Splash6 extends React.Component {
   render() {
     return (
       <>
-        <div className="splash6-content">
-          <div className="splash6-images">Place Images here</div>
+        <div className="splash-content">
           <div className="splash6-text">
-            <div className="splash6-hook">You already know the work.</div>
+            <div className="splash6-hook">
+              Now that you know the score, what are you waiting for?
+            </div>
             <div className="splash6-description">
-              Once you sign on with a guide, all of its quest notices show up in
-              your dashboard, just like you'd expect. See something great? Grab
-              a copy for your quest log. Add some notes if you like. Put your
-              own spin on it. Other adventurers will surely do the same to your
-              posts. That's how you build a reputation here.
+              Your destiny awaits. Welcome to Adventr.
             </div>
           </div>
+          <Link to="/signup">
+            <button className="signup">Get Started</button>
+          </Link>
+          <Link to="/login">
+            <button className="login">Login</button>
+          </Link>
         </div>
       </>
     );
