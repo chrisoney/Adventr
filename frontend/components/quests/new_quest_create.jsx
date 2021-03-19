3ed59f0c34ecee65ace95d40242e3bb8e6fce6a2
chrisoney <christopher.oney@yahoo.com>
2021-03-18 21:28:39 -0700

    change quest create configuration to match tumblr

--- frontend/components/quests/new_quest_create.jsx
+++ frontend/components/quests/new_quest_create.jsx
@@ -11,28 +11,33 @@ class NewQuestCreate extends React.Component {
     const { openModal } = this.props;
     return (
       <div className="quest-create-container">
-        <button className="text-container" onClick={() => this.props.openModal("new-text")}>
-            <div className="text-button fas fa-font"></div>
-            <span>Text</span>
-				</button>
+        <div className="create-avatar">
+          
+        </div>
+        <div className="quest-create-button-container">
+          <button className="text-container" onClick={() => this.props.openModal("new-text")}>
+              <div className="text-button fas fa-font"></div>
+              <span>Text</span>
+          </button>
 
-        <button className="image-container" onClick={() => this.props.openModal("new-image")}>
-            <div className="image-button fas fa-camera"></div>
-            <span>Image</span>
-        </button>
+          <button className="image-container" onClick={() => this.props.openModal("new-image")}>
+              <div className="image-button fas fa-camera"></div>
+              <span>Image</span>
+          </button>
 
-        <button className="quote-container" onClick={() => this.props.openModal("new-quote")}>
-            <div className="quote-button fas fa-quote-right"></div>
-            <span>Quote</span>
-        </button>
-        <button className="audio-container" onClick={() => this.props.openModal("new-audio")}>
-            <div className="audio-button fas fa-music"></div>
-            <span>Audio</span>
-        </button>
-        <button className="video-container" onClick={() => this.props.openModal("new-video")}>
-          <div className="video-button fas fa-video"></div>
-          <span>Video</span>
-        </button>
+          <button className="quote-container" onClick={() => this.props.openModal("new-quote")}>
+              <div className="quote-button fas fa-quote-right"></div>
+              <span>Quote</span>
+          </button>
+          <button className="audio-container" onClick={() => this.props.openModal("new-audio")}>
+              <div className="audio-button fas fa-music"></div>
+              <span>Audio</span>
+          </button>
+          <button className="video-container" onClick={() => this.props.openModal("new-video")}>
+            <div className="video-button fas fa-video"></div>
+            <span>Video</span>
+          </button>
+        </div>
       </div>
     )
   }
