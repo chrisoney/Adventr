acb195e5c4fbc018f88e17b87af4558d98bff6e9
chrisoney <christopher.oney@yahoo.com>
2021-03-26 23:46:35 -0700

    start sorting into columns for explore page

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -5,13 +5,25 @@ import NewQuestCreate from '../quests/new_quest_create'
 class Explore extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      windowSize: 0,
+    }
+    this.getWindowDimensions = this.getWindowDimensions.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchAllQuests();
     this.props.fetchAllFollows();
     this.props.fetchAllLikes();
+    window.addEventListener('resize', this.getWindowDimensions)
+    this.getWindowDimensions();
   }
+
+  getWindowDimensions() {
+    const { innerWidth } = window;
+    this.setState({ windowSize: innerWidth })
+  }
+
 
   componentDidUpdate(prevProps){
 		if (this.props.quests.length !== prevProps.quests.length){
@@ -25,6 +37,45 @@ class Explore extends React.Component {
       <QuestContainer key={idx} quest={quest} loc={"dash"}/>
     ))
     questList = questList.reverse();
+
+    let questDisplay;
+    if (this.state.windowSize > 1300) {
+      console.log("-----------------column 3----------------------")
+      questDisplay = (
+        <div className="quest-columns">
+          <div className="quest-column-3">
+            {questList.filter((quest, idx) => idx % 3 === 0)}
+          </div>
+          <div className="quest-column-3">
+            {questList.filter((quest, idx) => idx % 3 === 1)}
+          </div>
+          <div className="quest-column-3">
+            {questList.filter((quest, idx) => idx % 3 === 2)}
+            </div>
+          </div>
+      )
+    } else if (this.state.windowSize > 1000) {
+      console.log("-----------------column 2----------------------")
+      questDisplay = (
+        <div className="quest-columns">
+          <div className="quest-column-2">
+            {questList.filter((quest, idx) => idx % 2 === 0)}
+          </div>
+          <div className="quest-column-2">
+            {questList.filter((quest, idx) => idx % 2 === 1)}
+          </div>
+        </div>
+      )
+    } else {
+      console.log("-----------------column 1----------------------")
+      questDisplay = (
+        <div className="quest-columns">
+          <div className="quest-column-1">
+            {questList}
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="dashboard-container">
         <div className="explore">
@@ -33,7 +84,7 @@ class Explore extends React.Component {
             <p className="dash-welcome">Welcome back to Adventr!</p> */}
           </div>
           <NewQuestCreate currentUser={currentUser} />
-          {questList}
+          {questDisplay}
         </div>
       </div>
     );
