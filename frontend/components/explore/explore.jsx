ca5c0d91fac97fa6787e779dd76d32dd531cd65c
chrisoney <christopher.oney@yahoo.com>
2021-03-27 19:52:58 -0700

    add sample tags and tab switch

--- frontend/components/explore/explore.jsx
+++ frontend/components/explore/explore.jsx
@@ -78,8 +78,103 @@ class Explore extends React.Component {
       <div className="explore-container">
         <div className="explore">
           <div className="explore-left">
-            <div className="explore-header"></div>
-            <div className="explore-tag-container"></div>
+            <div className="explore-header">
+              {/* Placeholders, no true functionality yet */}
+              <div className="explore-tab">For You</div>
+              <div className="explore-tab">Trending</div>
+              <div className="explore-tab">Guildmaster Picks</div>
+              <div className="explore-tab">More</div>
+            </div>
+            <div className="explore-tag-container">
+              {/* Tag 1 */}
+              <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Warriors</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/river_world.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/circle_sample.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              </div>
+               {/* Tag 2 */}
+               <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Clerics</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/dark_forest.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/tree_house.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              {/* Tag 3 */}
+              <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Rangers</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/river_world.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/circle_sample.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              </div>
+               {/* Tag 4 */}
+               <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Warlocks</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/dark_forest.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/tree_house.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              {/* Tag 5 */}
+              <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Wizards</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/river_world.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/circle_sample.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              </div>
+               {/* Tag 6 */}
+               <div className="explore-new-tag-container">
+                <div className="new-tag-title">#Rogues</div>
+                <div className="new-tag-examples">
+                  <img
+                    src="/assets/dark_forest.jpg"
+                    className="new-tag-example-image"
+                  />
+                  <img
+                    src="/assets/tree_house.jpg"
+                    className="new-tag-example-image"
+                  />
+                </div>
+                <div className="new-tag-follow-button">Follow</div>
+              </div>
+            </div>
             {questDisplay}
           </div>
           <div className="explore-right">
