48b4c976931c70e4c385aae5d8eda2882c490a3a
chrisoney <christopher.oney@yahoo.com>
2021-07-05 16:53:31 -0700

    attempt to reuse new quest container for reblog

--- frontend/components/modal/new_quest_container.js
+++ frontend/components/modal/new_quest_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import NewQuestForm from './new_quest_form'
 import { fetchAllQuests, createQuest, addTagToQuest } from '../../actions/quest_actions';
+import { createReblog, addTagToReblog } from '../../actions/reblog_actions';
 import { fetchAllTags, createTag } from '../../actions/tag_actions';
 import { closeModal } from '../../actions/modal_actions'
 
@@ -9,12 +10,15 @@ const mapStateToProps = (state, ownProps) => {
     currentUser: state.session.currentUser,
     tags: Object.values(state.entities.tags),
     type: ownProps.type,
-    task: 'create'
+    task: 'create',
+    quest: ownProps.quest || null,
   };
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  questSubmitAction: (quest) => dispatch(createQuest(quest)),
+  questSubmitAction: ownProps.quest ?
+    (quest) => dispatch(createQuest(quest)) :
+    (reblog) => dispatch(createReblog(reblog)),
   fetchAllTags: () => dispatch(fetchAllTags()),
   fetchAllQuests: () => dispatch(fetchAllQuests()),
   createTag: (tag) => dispatch(createTag(tag)),
