fbc030484320786e6419ac203de3e19f98aa2440
chrisoney <christopher.oney@yahoo.com>
2021-09-16 12:12:24 -0700

    testing dragon logo

--- frontend/components/loading/loading.jsx
+++ frontend/components/loading/loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+// import dragonImage from 'images/dragon-logo.png'
 
 const Loading = (props) => {
   return (
     <div className={props.background || 'black-container'}>
-      <img src="/assets/images/dragon-logo.png" className="dragon-loader" />
+      <img src='images/dragon-logo.png' className="dragon-loader" />
     </div>
   );
 };
